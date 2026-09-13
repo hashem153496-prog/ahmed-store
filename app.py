b8b8e1f230a46ab053e4459f8dc4f643475e3cf3
hashem153496-prog <hashem153496@gmail.com>
2026-09-13 05:03:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = os.getenv("SECRET_KEY", "change-this-in-production")
 app.config["MAX_CONTENT_LENGTH"] = 20 * 1024 * 1024
 
 db_url = os.getenv("DATABASE_URL", "sqlite:///store.db")
-if db_url.startswith("postgres://"):
+if db_url and db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
